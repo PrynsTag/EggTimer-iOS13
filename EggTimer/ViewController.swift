3eaa8a53da7fbc3b84f36ee369454983e087297f
PrynsTag <princevelasco16@gmail.com>
2021-05-22 22:29:56 +0800

    Completed.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+
+    var audioPlayer = AVAudioPlayer()
+    var countdownTimer = Timer()
+    var totalTime: Int = 0
+    var elapsedTime: Int = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        let eggTimes = ["Soft": 5, "Medium": 420, "Hard": 720]
+        
+        titleLabel.text = hardness
+        countdownTimer.invalidate()
+        elapsedTime = 0
+        
+        totalTime = eggTimes[hardness]!
+        countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
     
 
-
+    @objc func updateTime() {
+        if elapsedTime < totalTime {
+            print("\(totalTime - elapsedTime) seconds")
+            
+            elapsedTime += 1
+            progressView.progress = Float(elapsedTime) / Float(totalTime)
+        } else {
+            let alarm_sound = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            audioPlayer = try! AVAudioPlayer(contentsOf: alarm_sound!)
+            audioPlayer.play()
+            
+            titleLabel.text = "Done!"
+            countdownTimer.invalidate()
+        }
+    }
 }
+
+
